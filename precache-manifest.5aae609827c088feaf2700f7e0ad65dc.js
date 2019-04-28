e643e9147dc9165ecdb97dfe73629c0ee5e3bbd9
Lynne Rang <lynne.rang@gmail.com>
2019-04-28 13:56:19 -0600

    Updates

---
+++ precache-manifest.5aae609827c088feaf2700f7e0ad65dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af50b158e6169d200d138984a1ea5054",
+    "url": "/ideaboxReact/index.html"
+  },
+  {
+    "revision": "256a3445fb204ac77246",
+    "url": "/ideaboxReact/static/css/main.bf9fa4b7.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/ideaboxReact/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "256a3445fb204ac77246",
+    "url": "/ideaboxReact/static/js/main.4a9f3992.chunk.js"
+  },
+  {
+    "revision": "cbee2bcb25f1e35eb56d",
+    "url": "/ideaboxReact/static/js/runtime~main.b5bae32e.js"
+  },
+  {
+    "revision": "527a291180f9061897cc4c6b4c246515",
+    "url": "/ideaboxReact/static/media/delete.527a2911.svg"
+  },
+  {
+    "revision": "25ed1843647629adcdda4a0bc760ea2f",
+    "url": "/ideaboxReact/static/media/downvote.25ed1843.svg"
+  },
+  {
+    "revision": "7c98ec6226fd3bc32e3c32f556a2f735",
+    "url": "/ideaboxReact/static/media/mrpb.7c98ec62.png"
+  },
+  {
+    "revision": "0c69c4179545d2abf0fea37de72360d3",
+    "url": "/ideaboxReact/static/media/upvote.0c69c417.svg"
+  }
+]);
